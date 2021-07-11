83314faf70c6e059c0b88170260f2c87ed56466f
Hosein Abolhasani <hosein.abolhasani1998@gmail.com>
2021-07-11 17:49:08 +0430

    add search bar and see recipe section

--- screens/Home.js
+++ screens/Home.js
@@ -9,9 +9,86 @@ import {
   Image,
 } from 'react-native';
 import CategoryCard from '../components/CategoryCard';
-import { COLORS, dummyData, SIZES } from '../constants';
+import { COLORS, dummyData, FONTS, icons, images, SIZES } from '../constants';
 
 const Home = ({ navigation }) => {
+  function renderHeader() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          marginHorizontal: SIZES.padding,
+          alignItems: 'center',
+          height: 80,
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Text style={{ color: COLORS.darkGreen, ...FONTS.h2 }}>
+            Hello Hosein
+          </Text>
+          <Text style={{ marginTop: 3, color: COLORS.gray, ...FONTS.body4 }}>
+            What you want to cook today?
+          </Text>
+        </View>
+        <TouchableOpacity onPress={() => console.log('profiel')}>
+          <Image
+            source={images.profile}
+            style={{ width: 40, height: 40, borderRadius: 20 }}
+          />
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
+  function renderSearchBar() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          height: 50,
+          alignItems: 'center',
+          marginHorizontal: SIZES.padding,
+          paddingHorizontal: SIZES.padding,
+          borderRadius: 10,
+          backgroundColor: COLORS.lightGray,
+        }}
+      >
+        <Image
+          source={icons.search}
+          style={{ width: 20, height: 20, tintColor: COLORS.gray }}
+        />
+        <TextInput
+          style={{ marginLeft: SIZES.radius, ...FONTS.body3, width: '100%' }}
+          placeholderTextColor={COLORS.gray}
+          placeholder="Search Recipes"
+        />
+      </View>
+    );
+  }
+
+  function renderSeeRecipeCard() {
+    return (
+      <View
+        style={{
+          flexDirection: 'row',
+          height: 100,
+          alignItems: 'center',
+          marginTop: SIZES.padding,
+          marginHorizontal: SIZES.padding,
+          paddingHorizontal: SIZES.padding,
+          borderRadius: 10,
+          backgroundColor: COLORS.lightGreen,
+        }}
+      >
+        <Image source={images.recipe} style={{ width: 80, height: 80 }} />
+
+        <View
+          style={{ width: 100, alignItems: 'center', justifyContent: 'center' }}
+        ></View>
+      </View>
+    );
+  }
+
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.white }}>
       <FlatList
@@ -19,7 +96,20 @@ const Home = ({ navigation }) => {
         keyExtractor={(item) => `${item.id}`}
         keyboardDismissMode="on-drag"
         showsVerticalScrollIndicator
-        ListHeaderComponent={<View></View>}
+        ListHeaderComponent={
+          <View>
+            {/* Header */}
+            {renderHeader()}
+
+            {/* Search Bar */}
+            {renderSearchBar()}
+            {/* See Recipe Card */}
+            {renderSeeRecipeCard()}
+            {/* Trending Section */}
+
+            {/* Category Header */}
+          </View>
+        }
         renderItem={({ item }) => (
           <CategoryCard
             category={item}
